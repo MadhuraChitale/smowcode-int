4f9610f33a51daa4ceefc6ee5b9393e340908c6b
MadhuraChitale <114588327+MadhuraChitale@users.noreply.github.com>
2023-10-17 18:17:36 +0530

    last commit

--- pbrlm1.java
+++ pbrlm1.java
@@ -1,51 +1,40 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Scanner;
 
-class problem1 {
-    public static int minimumswapsaps(int[] number) {
-        int[] t = new int[number.length * 2];
+public class pbrlm1 {
 
-        for (int i = 0; i < t.length; ++i) {
-            t[i] = number[i % number.length];
-        }
-
-        int win = 0;
-        for (int num : number) {
-            if (num == 1)
-                win++;
-        }
-
-        if (win == number.length)
-            return 0;
-        int minimumswapsaps = Integer.MAX_VALUE;
-        int l = 0, r = 0;
-        int cone = 0;
-        while (r < number.length + win) {
-            if (t[r] == 1)
-                cone++;
-
-            if (r - l + 1 > win) {
-                if (t[l] == 1)
-                    cone--;
-                l++;
+    public static int returnMinSwaps(int size, int[] array) {
+        int numOnes = 0;
+        int minSwapsCount = 0;
+        for (int element : array) {
+            if (element == 0) {
+                numOnes++;
             }
-
-            if (r - l + 1 == win) {
-                int currentWinSwapNeeded = win - cone;
-                minimumswapsaps = Math.min(minimumswapsaps, currentWinSwapNeeded);
-            }
-            r++;
         }
-
-        return minimumswapsaps;
+        for (int i = 0; i < size; i++) {
+            if (i > numOnes)
+                break;
+            if (array[i] == 1)
+                minSwapsCount++;
+        }
+        return minSwapsCount;
     }
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr[] = new int[n];
-        for (int g = 0; g < n; g++) {
-            arr[g] = sc.nextInt();
+    public static void minSwaps() {
+        Scanner inputScanner = new Scanner(System.in);
+        int arraySize = inputScanner.nextInt();
+        int[] elements = new int[arraySize];
+        for (int i = 0; i < arraySize; i++) {
+            elements[i] = inputScanner.nextInt();
         }
-        System.out.println(minimumswapsaps(arr));
+        System.out.println(returnMinSwaps(arraySize, elements));
+        inputScanner.close();
     }
+
+    public static void main(String[] args) {
+        minSwaps();
+    }
+
 }
